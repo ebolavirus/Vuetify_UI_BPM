98034da9af686069ee4058afab8ae9d5150ab29b
jkjiao <jkjiao@whchem.com>
2019-08-28 16:05:17 +0800

    222 Signed-off-by: jkjiao <jkjiao@whchem.com>

---
+++ src/components/test.js
@@ -0,0 +1,83 @@
+import Vue from 'vue'
+import WHLogo from './singles/WHLogo'
+import WHIcon from './singles/WHIcon'
+import WHHyperlink from './singles/WHHyperlink'
+import WHTextfield from './singles/WHTextfield'
+import WHNumberfield from './singles/WHNumberfield'
+import WHMoneyfield from './singles/WHMoneyfield'
+import WHTextarea from './singles/WHTextarea'
+import WHButton from './singles/WHButton'
+import WHSelect from './singles/WHSelect'
+import WHRadioGroup from './singles/WHRadioGroup'
+import WHRadio from './singles/WHRadio'
+import WHCheckbox from './singles/WHCheckbox'
+import WHTreeview from './singles/WHTreeview'
+import WHTimeline from './singles/WHTimeline'
+import WHTimelineItem from './singles/WHTimelineItem'
+import WHTab from './singles/WHTab'
+import WHTabItem from './singles/WHTabItem'
+import WHTabsItems from './singles/WHTabsItems'
+import WHTabs from './singles/WHTabs'
+import WHTabsSlider from './singles/WHTabsSlider'
+
+import WHDatePicker from './composites/WHDatePicker'
+import WHTimePicker from './composites/WHTimePicker'
+import WHPagination from './composites/WHPagination'
+import WHTable from './composites/WHTable'
+import WHEditdialog from './composites/WHEditdialog'
+import WHLoading from './composites/WHLoading'
+
+import WHApp from './layout/WHApp'
+import WHContent from './layout/WHContent'
+import WHCard from './layout/WHCard'
+import WHCardText from './layout/WHCardText'
+import WHCardTitle from './layout/WHCardTitle'
+import WHCardActions from './layout/WHCardActions'
+import WHDialog from './layout/WHDialog'
+import WHDrawer from './layout/WHDrawer'
+import WHFooter from './layout/WHFooter'
+import WHTopbar from './layout/WHTopbar'
+import WHLayout from './layout/WHLayout'
+import WHFlex from './layout/WHFlex'
+
+Vue.component(WHLogo.name, WHLogo)
+Vue.component(WHIcon.name, WHIcon)
+Vue.component(WHHyperlink.name, WHHyperlink)
+Vue.component(WHTextfield.name, WHTextfield)
+Vue.component(WHNumberfield.name, WHNumberfield)
+Vue.component(WHMoneyfield.name, WHMoneyfield)
+Vue.component(WHTextarea.name, WHTextarea)
+Vue.component(WHButton.name, WHButton)
+Vue.component(WHSelect.name, WHSelect)
+Vue.component(WHRadioGroup.name, WHRadioGroup)
+Vue.component(WHRadio.name, WHRadio)
+Vue.component(WHCheckbox.name, WHCheckbox)
+Vue.component(WHTreeview.name, WHTreeview)
+Vue.component(WHTimeline.name, WHTimeline)
+Vue.component(WHTimelineItem.name, WHTimelineItem)
+
+Vue.component(WHTab.name, WHTab)
+Vue.component(WHTabsItems.name, WHTabsItems)
+Vue.component(WHTabItem.name, WHTabItem)
+Vue.component(WHTabs.name, WHTabs)
+Vue.component(WHTabsSlider.name, WHTabsSlider)
+Vue.component(WHDatePicker.name, WHDatePicker)
+Vue.component(WHTimePicker.name, WHTimePicker)
+Vue.component(WHTable.name, WHTable)
+Vue.component(WHEditdialog.name, WHEditdialog)
+
+Vue.component(WHPagination.name, WHPagination)
+Vue.component(WHLoading.name, WHLoading)
+
+Vue.component(WHApp.name, WHApp)
+Vue.component(WHContent.name, WHContent)
+Vue.component(WHCard.name, WHCard)
+Vue.component(WHCardText.name, WHCardText)
+Vue.component(WHCardTitle.name, WHCardTitle)
+Vue.component(WHCardActions.name, WHCardActions)
+Vue.component(WHDialog.name, WHDialog)
+Vue.component(WHDrawer.name, WHDrawer)
+Vue.component(WHFooter.name, WHFooter)
+Vue.component(WHTopbar.name, WHTopbar)
+Vue.component(WHLayout.name, WHLayout)
+Vue.component(WHFlex.name, WHFlex)
