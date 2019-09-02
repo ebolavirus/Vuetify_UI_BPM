29b7ef1ea67f9d01da0ca0c87c56673eb0bffb93
MingmingSun <xdog2@163.com>
2019-09-02 15:01:54 +0800

    sidedrawer logic.

--- src/demo/components/Sidedrawer.vue
+++ src/demo/components/Sidedrawer.vue
@@ -1,69 +1,57 @@
 <template>
-  <v-navigation-drawer app v-model="drawer" dark>
-    <v-expansion-panels v-model="panel" :accordion="true" :multiple="true">
-      <v-expansion-panel>
-        <v-expansion-panel-header>
-          基本信息
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-text-field v-for="(item, index) in desserts" :key="index" :titleValue="item.name"
-            :realValue="item.calories">
-          </v-text-field>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-      <v-expansion-panel>
-        <v-expansion-panel-header>
-          审批历史
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-timeline dense>
-            <v-timeline-item color="amber lighten-1" fill-dot right small>
-              <v-card>
-                <v-card-title class="amber">
-                  <span class="white--text">当前环节</span>
-                </v-card-title>
-                <v-container>
-                  <v-layout>
-                    <v-flex>
-                      孙明明(mmsuna)<br>单一签核
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-timeline-item>
-            <v-timeline-item color="green lighten-1" fill-dot right>
-              <v-card>
-                <v-card-title class="green lighten-1">
-                  <span class="white--text">起草-提交</span>
-                </v-card-title>
-                <v-container>
-                  <v-layout>
-                    <v-flex>
-                      孙明明(mmsuna)<br>2019-7-19 10:36:26<br>用时0天<br>同意
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-timeline-item>
-            <v-timeline-item color="green lighten-1" fill-dot right>
-              <v-card>
-                <v-card-title class="green lighten-1">
-                  <span class="white--text">起草-提交</span>
-                </v-card-title>
-                <v-container>
-                  <v-layout>
-                    <v-flex>
-                      孙明明(mmsuna)<br>2019-7-19 10:36:26<br>用时0天<br>同意
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-timeline-item>
-          </v-timeline>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-navigation-drawer>
+  <wh-drawer app v-model="drawer" dark>
+    <wh-area-panels>
+      <wh-area-panel title="基本信息">
+        <wh-textfield v-for="(item, index) in desserts" :key="index" :label="item.name" :value="item.calories" />
+      </wh-area-panel>
+      <wh-area-panel title="审批历史">
+        <wh-timeline dense>
+          <wh-timeline-item color="amber lighten-1" fill-dot right small>
+            <wh-card>
+              <wh-card-title class="amber">
+                <span class="white--text">当前环节</span>
+              </wh-card-title>
+              <wh-container>
+                <wh-layout>
+                  <wh-flex>
+                    孙明明(mmsuna)<br>单一签核
+                  </wh-flex>
+                </wh-layout>
+              </wh-container>
+            </wh-card>
+          </wh-timeline-item>
+          <wh-timeline-item color="green lighten-1" fill-dot right>
+            <wh-card>
+              <wh-card-title class="green lighten-1">
+                <span class="white--text">起草-提交</span>
+              </wh-card-title>
+              <wh-container>
+                <wh-layout>
+                  <wh-flex>
+                    孙明明(mmsuna)<br>2019-7-19 10:36:26<br>用时0天<br>同意
+                  </wh-flex>
+                </wh-layout>
+              </wh-container>
+            </wh-card>
+          </wh-timeline-item>
+          <wh-timeline-item color="green lighten-1" fill-dot right>
+            <wh-card>
+              <wh-card-title class="green lighten-1">
+                <span class="white--text">起草-提交</span>
+              </wh-card-title>
+              <wh-container>
+                <wh-layout>
+                  <wh-flex>
+                    孙明明(mmsuna)<br>2019-7-19 10:36:26<br>用时0天<br>同意
+                  </wh-flex>
+                </wh-layout>
+              </wh-container>
+            </wh-card>
+          </wh-timeline-item>
+        </wh-timeline>
+      </wh-area-panel>
+    </wh-area-panels>
+  </wh-drawer>
 </template>
 
 <script>
