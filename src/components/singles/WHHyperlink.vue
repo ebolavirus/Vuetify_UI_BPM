25bf519443afb9c5136fa54bc7ae4018ba94e917
MingmingSun <xdog2@163.com>
2019-09-09 17:35:24 +0800

    add a title label for hyperlink.

--- src/components/singles/WHHyperlink.vue
+++ src/components/singles/WHHyperlink.vue
@@ -1,5 +1,15 @@
 <template>
-  <a :href="href" target="_blank">{{showlabel}}</a>
+  <div class="v-input v-input--is-label-active v-text-field" label="申请日期">
+    <div class="v-input__slot">
+      <div class="v-text-field__slot">
+        <label for="input-907" class="v-label v-label--active theme--light"
+          style="left: 0px; right: auto; position: absolute;">{{label}}</label>
+        <!-- <input id="input-907" type="text"> -->
+        <a :href="href" target="_blank">{{showlabel}}</a>
+      </div>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
