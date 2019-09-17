5f520f8b27a4b86e286dc5cbc776cf7ffdc1641a
SunMingming <xdog2@163.com>
2019-09-17 15:16:32 +0800

    2 fogotten label issue.

--- src/components/singles/WHHyperlink.vue
+++ src/components/singles/WHHyperlink.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="v-input v-input--is-label-active v-text-field" label="申请日期">
+  <div class="v-input v-input--is-label-active v-text-field" :label="label">
     <div class="v-input__slot">
       <div class="v-text-field__slot">
         <label for="input-907" class="v-label v-label--active theme--light"
