d66d68a0b74af8669286f572c3f07e50811d75cf
MingmingSun <xdog2@163.com>
2019-09-25 11:01:24 +0800

    snackbar, remove closeText and replace with close icon.

--- src/components/SimpleSnackbar.vue
+++ src/components/SimpleSnackbar.vue
@@ -1,42 +1,40 @@
 <template>
-    <v-snackbar v-model="shown" :color="color" :bottom="snackbarY === 'bottom'" :left="snackbarX === 'left'"
-        :right="snackbarX === 'right'" :timeout="snackbarTimeout" :top="snackbarY === 'top'">
-        {{ text }}
-        <v-btn text :ripple="false" @click="close()">
-            {{ closeText }}
-        </v-btn>
-    </v-snackbar>
+  <v-snackbar v-model="shown" :color="color" :bottom="snackbarY === 'bottom'" :left="snackbarX === 'left'"
+    :right="snackbarX === 'right'" :timeout="snackbarTimeout" :top="snackbarY === 'top'">
+    {{ text }}
+    <v-btn icon :ripple="false" @click="close()">
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+  </v-snackbar>
 </template>
-
 <script>
-    export default {
-        name: "Snackbar",
-        props: {
-            snackbarX: String,
-            snackbarY: String,
-            color: String,
-            snackbarTimeout: Number,
-            closeText: String,
-            text: String
-        },
-        data() {
-            return {
-                shown: true
-            }
-        },
-        methods: {
-            show() {
-                this.shown = true;
-                if (this.timeout) {
-                    setTimeout(() => this.$emit("close"), this.timeout + 300);
-                }
-            },
-            close() {
-                this.shown = false;
-                setTimeout(() => this.$emit("close"), 500);
-            }
+  export default {
+    name: "Snackbar",
+    props: {
+      snackbarX: String,
+      snackbarY: String,
+      color: String,
+      snackbarTimeout: Number,
+      text: String
+    },
+    data() {
+      return {
+        shown: true
+      }
+    },
+    methods: {
+      show() {
+        this.shown = true;
+        if (this.timeout) {
+          setTimeout(() => this.$emit("close"), this.timeout + 300);
         }
+      },
+      close() {
+        this.shown = false;
+        setTimeout(() => this.$emit("close"), 500);
+      }
     }
+  }
 </script>
 
 <style scoped>
