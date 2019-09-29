c0a10268a1f0300b01c87873d162c15379a93981
SunMingming <xdog2@163.com>
2019-09-29 16:08:53 +0800

    minify the vuetify.css.

--- src/main.js
+++ src/main.js
@@ -10,23 +10,23 @@ import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loade
 import './css/vuetify.css'
 
 function _getLocale() {
-    let locale = "en";
-    const locales = Object.keys(i18n);
-    for (let i = 0; i < locales.length; i++) {
-        if (window.location.pathname.indexOf('/' + locales[i] + '/') !== -1) {
-            locale = locales[i];
-            return locale;
-        }
+  let locale = "en";
+  const locales = Object.keys(i18n);
+  for (let i = 0; i < locales.length; i++) {
+    if (window.location.pathname.indexOf('/' + locales[i] + '/') !== -1) {
+      locale = locales[i];
+      return locale;
     }
-    return locale;
+  }
+  return locale;
 }
 
 Vue.use(DialogPromise, {
-    snackbarParent: "app",
-    locale: _getLocale()
+  snackbarParent: "app",
+  locale: _getLocale()
 });
 Vue.config.productionTip = false;
 new Vue({
-    vuetify,
-    render: h => h(App),
+  vuetify,
+  render: h => h(App),
 }).$mount('#app');
