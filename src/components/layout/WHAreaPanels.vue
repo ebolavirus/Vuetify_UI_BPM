5bc776a7f13461f05f88fb874cc81d5686635855
SunMingming <xdog2@163.com>
2019-10-14 14:52:19 +0800

    pagination add prop for values.

--- src/components/layout/WHAreaPanels.vue
+++ src/components/layout/WHAreaPanels.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-expansion-panels multiple v-model="expandeditems" :inheritAttrs="false">
+  <v-expansion-panels multiple :value="value" :inheritAttrs="false">
     <slot />
   </v-expansion-panels>
 </template>
@@ -7,9 +7,12 @@
 <script>
   export default {
     name: 'wh-area-panels',
-    data() {
-      return {
-        expandeditems: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+    props: {
+      value: {
+        type: Array,
+        default () {
+          return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+        }
       }
     }
   }
