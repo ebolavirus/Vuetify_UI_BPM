caee31bd5b906e8b0230c39ecfb117f157d816a0
gysoft <526584085@qq.com>
2019-11-07 10:07:47 +0800

    1

--- src/components/layout/WHDialog.vue
+++ src/components/layout/WHDialog.vue
@@ -1,16 +1,27 @@
 <template>
-    <v-dialog :fullscreen="!this.$vuetify.breakpoint.smAndUp" v-bind="$attrs" v-on="$listeners" :inheritAttrs="false" >
-        <v-toolbar v-if="!this.$vuetify.breakpoint.smAndUp" dark color="primary">
+    <v-dialog :fullscreen="!$vuetify.breakpoint.smAndUp" v-bind="$attrs" v-on="$listeners" :inheritAttrs="false" >
+        <v-toolbar v-if="$vuetify.breakpoint.smAndUp?isShowHeader:true" dark color="primary">
             <v-btn icon dark @click="close">
                 <v-icon>mdi-close</v-icon>
             </v-btn>
+            <v-toolbar-title class="title">{{title}}</v-toolbar-title>
         </v-toolbar>
         <slot></slot>
     </v-dialog>
 </template>
 <script>
   export default {
-    name: 'wh-dialog',
+      name: 'wh-dialog',
+      props:{
+          title: {
+              type: String,
+              default: ''
+          },
+          isShowHeader: {
+              type: Boolean,
+              default: false
+          }
+      },
       methods:{
         close(){
             this.$emit('input',false)
