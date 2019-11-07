b6e30093809f1998d7c4bf32f7ade363404b5a75
gysoft <526584085@qq.com>
2019-11-07 09:03:08 +0800

    1

--- src/components/layout/WHDialog.vue
+++ src/components/layout/WHDialog.vue
@@ -1,9 +1,20 @@
+<template>
+    <v-dialog :fullscreen="!this.$vuetify.breakpoint.smAndUp" v-bind="$attrs" v-on="$listeners" :inheritAttrs="false" >
+        <v-toolbar v-if="!this.$vuetify.breakpoint.smAndUp" dark color="primary">
+            <v-btn icon dark @click="close">
+                <v-icon>mdi-close</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <slot></slot>
+    </v-dialog>
+</template>
 <script>
-  import {
-    VDialog
-  } from 'vuetify/lib'
   export default {
     name: 'wh-dialog',
-    extends: VDialog
+      methods:{
+        close(){
+            this.$emit('input',false)
+        }
+      }
   };
 </script>
